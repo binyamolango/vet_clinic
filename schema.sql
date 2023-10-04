f92b7befd428fcc04aa343fd73539fc7d4163802
binyamolango <olangobinyam@gmail.com>
2023-10-04 17:18:42 +0300

    Add foreign keys to the animals table

--- schema.sql
+++ schema.sql
@@ -1,12 +1,28 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(15) NOT NULL,
-    date_of_birth DATE NOT NULL,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(15),
+    date_of_birth DATE,
     escape_attempts INT,
-    neutered BOOLEAN NOT NULL,
-    weight_kg DECIMAL
+    neutered BOOLEAN,
+    weight_kg NUMERIC(10,2)
 );
 
 ALTER TABLE animals ADD species VARCHAR(50);
+
+CREATE TABLE owners (
+    id BIGSERIAL,
+    full_name VARCHAR(30),
+    age INT,
+    PRIMARY KEY(id) 
+);
+
+CREATE TABLE species (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT, ADD CONSTRAINT fk_species_id FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_id INT, ADD CONSTRAINT fk_owners_id FOREIGN KEY (owners_id) REFERENCES owners(id);
